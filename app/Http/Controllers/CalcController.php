1b3a878bdc74bb2997495d7f7c7f163e0265f1f3
YukiOgaki <teamgift8@gmail.com>
2025-01-12 22:10:30 +0900

    計算アプリ課題完了

---
+++ app/Http/Controllers/CalcController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalcController extends Controller
+{
+    public function calcs($value1, $operator, $value2)
+    {
+        if ($operator == 'addition') {
+            $result = $value1 + $value2;
+        } elseif ($operator == 'subtraction') {
+            $result = $value1 - $value2;
+        } elseif ($operator == 'multiplication') {
+            $result = $value1 * $value2;
+        } else {
+            $result = $value1 / $value2;
+        }
+        return view('message.calcs', ['result' => $result, 'value1' => $value1, 'value2' => $value2, 'operator' => $operator]);
+    }
+}
